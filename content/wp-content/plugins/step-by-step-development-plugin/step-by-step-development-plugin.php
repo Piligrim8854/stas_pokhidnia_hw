2baf93f67421b14b817c698f45064a19cbf2bba8
Piligrim8854 <Piligrim8854@gmail.com>
2017-11-24 17:05:45 +0200

    Начало создания плагина stes-by-step

---
+++ content/wp-content/plugins/step-by-step-development-plugin/step-by-step-development-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: posta-step-by-step-plugin
+Plugin URI: http://www.sg2lessons2.ho.ua/
+Description: Это плагин написаный в рамках обучению работе с word press. Выкладывать его в сеть я не собираюсь, по этому если он вам не понравился, то в принципе - это ваши личные проблеммы. А вообще лучше со мной свяжитесь. И да, поддерживать его я точно не собираюсь.
+Version: Beta version 0.1
+Author: Sirius Posta
+Author URI: http://www.sg2lessons2.ho.ua/
+*/
+?>
