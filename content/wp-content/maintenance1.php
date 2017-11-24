d75ec4b0415ae0a0c92d655aa0839373ebf14d2a
Piligrim8854 <Piligrim8854@gmail.com>
2017-11-24 15:58:14 +0200

    добавлены файлы .maintenance в корень и вп-контент

---
+++ content/wp-content/maintenance1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Sorry, we upgrading</title>
+		<meta charset="utf-8">
+		<style type="text/css">
+			body{
+				background-color: rgb(0, 128, 0);
+			}
+			.tok{
+				height: 45vh;
+				display: block;
+			}
+			h1{
+				margin: 0;
+				display: block;
+				text-align: center;
+				color: rgb(255, 255, 255);
+				background-color: rgb(255, 0, 0);
+			}
+		</style>
+	</head>
+	<body>
+		<div class="tok">.</div>
+		<h1>Sorry, we upgrading. Try later</h1>
+		<div class="tok">.</div>
+	</body>
+</html>
